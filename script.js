96bf7773207a0a0f9fd5c281039a8fda421ffe34
izabella braga <izabella.braga@eaportal.org>
2024-06-09 06:52:30 +0000

    ajustes da funcao de selecionar assunto para funcionar em todos os dispositivos

--- script.js
+++ script.js
@@ -7,3 +7,14 @@ botaoTema.addEventListener("click", () => {
 })
 
 verificarTema(body, botaoTema)
+const botoesAssunto = document.querySelectorAll(".assuntos button")
+botoesAssunto.forEach(botao => {
+    botao.addEventListener("click", selecionarAssunto)
+})
+
+function selecionarAssunto(evento){
+    const classeBotao = evento.target.className
+    const assunto = document.querySelector(`.${classeBotao} span`).innerText
+    localStorage.setItem("assunto", assunto)
+    window.location.href = "./pages/quiz/quiz.html"
+}
